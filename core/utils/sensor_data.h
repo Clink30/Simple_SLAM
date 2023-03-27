0eba63e6256b25c72a2936e191cbff2a7a6b047a
Clink30 <caiprivate@163.com>
2023-03-28 00:40:59 +0800

    Changes to be committed: 	new file:   core/utils/sensor_data.h

---
+++ core/utils/sensor_data.h
@@ -0,0 +1,33 @@
+#ifndef SENSOR_DATA_H
+#define SENSOR_DATA_H
+
+#include <Eigen/Eigen>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+namespace my_slam {
+
+    struct ImuData
+    {
+        double timestamp;
+
+        Eigen::Matrix<double, 3, 1> wm;
+
+        Eigen::Matrix<double, 3, 1> am;
+
+        bool operator<(const ImuData &other) { return timestamp < other.timestamp; }
+    };
+
+    struct CameraData
+    {
+        double timestamp;
+
+        std::vector<cv::Mat> images;
+
+        bool operator<(const ImuData &other) { return timestamp < other.timestamp; }
+    };
+        
+} // namespace ov_core
+
+
+#endif
